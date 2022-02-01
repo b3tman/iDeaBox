8e85ff687214af95f102eed31bff801764430840
b3tman <b3tmanann@gmail.com>
2022-02-01 16:30:54 +0300

    Create initial folders.

--- iDeaBox/iDeaBox/Presentation/Modules/Main/MainViewController.swift
+++ iDeaBox/iDeaBox/Presentation/Modules/Main/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  iDeaBox
 //
 //  Created by Maxim Brishten on 1.02.22.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
