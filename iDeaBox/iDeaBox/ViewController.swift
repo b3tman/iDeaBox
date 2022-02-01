4cb969862c86a8271f7ee2158454a2948143ca5a
b3tman <b3tmanann@gmail.com>
2022-02-01 14:10:11 +0300

    Init project.

---
+++ iDeaBox/iDeaBox/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  iDeaBox
+//
+//  Created by Maxim Brishten on 1.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
